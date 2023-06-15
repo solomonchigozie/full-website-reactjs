197e2deeb4c6da31cd6d0f1e09055bbd91e184f8
solomon chigozie <solomon1chigozie@gmail.com>
2023-06-15 17:04:22 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {
+    CloudUploadIcon,
+    DatabaseIcon,
+    PaperAirplaneIcon,
+    ServerIcon,
+} from '@heroicons/react/outline'
+
+function Hero() {
+  return (
+    <div>Hero</div>
+  )
+}
+
+export default Hero
